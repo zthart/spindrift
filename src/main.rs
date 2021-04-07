4820ec371ae8d0c1285442e1908cc613b793b324
Zach Hart <zthart96@gmail.com>
2021-04-06 23:31:20 -0700

    image_to_html function for Droplet, shuffle some constants

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,13 @@ use serde::Deserialize;
 use serde_yaml;
 use std::fs::File;
 use std::path::Path;
+use lazy_static::lazy_static;
 
 mod errors;
+
+const EM_REGEX_STR: &str = r"_(?P<em_text>.*?)_";
+const STRONG_REGEX_STR: &str = r"\*\*(?P<strong_text>.*?)\*\*";
+const A_REGEX_STR: &str = r"\[(?P<a_text>.*?)\]\((?P<a_href>.*?)\)";
 
 #[derive(Debug, Deserialize)]
 /// Metadata for a Droplet
@@ -44,6 +49,13 @@ struct Droplet {
     content: Option<String>,
 }
 
+lazy_static!{
+    // Unwrap these with certainty since the expressions themselves are constants (see above) and should compile just fine
+    static ref EM_REGEX: Regex = Regex::new(EM_REGEX_STR).unwrap();
+    static ref STRONG_REGEX: Regex = Regex::new(STRONG_REGEX_STR).unwrap();
+    static ref A_REGEX: Regex = Regex::new(A_REGEX_STR).unwrap();
+}
+
 impl Droplet {
     fn from_file(path: &Path) -> Result<Droplet, Error> {
         let droplet_file =
@@ -51,26 +63,45 @@ impl Droplet {
         Ok(serde_yaml::from_reader(droplet_file)?)
     }
 
-    fn content_to_html(&self, em_exp: Regex, strong_exp: Regex, a_exp: Regex) -> Option<String> {
+    fn image_to_html(&self) -> Option<String> {
+        if let Some(droplet_image) = &self.image {
+            let mut attr_vec: Vec<String> = Vec::new();
+            attr_vec.push(format!("src=\"{}\"", droplet_image.src.as_ref().display()));
+            droplet_image.alt.as_ref().map(|alt| {
+                let cleaned = alt.trim().split('\n').fold("".to_string(), |mut acc, alt_line| {
+                    acc.push_str(alt_line);
+                    acc
+                });
+
+                attr_vec.push(format!("alt=\"{}\"", cleaned));
+            });
+
+            Some(format!("<img {}/>\n", attr_vec.join(" ")))
+        } else {
+          None
+        }
+    }
+
+    fn content_to_html(&self) -> Option<String> {
         self.content.as_ref().map(|content| {
             content
                 .trim()
                 .split('\n')
                 .map(|v| {
-                    let mut builder = em_exp
+                    let mut builder = EM_REGEX
                         .replace_all(v, "<em>$em_text</em>")
                         .to_owned()
                         .to_string();
-                    builder = strong_exp
+                    builder = STRONG_REGEX
                         .replace_all(&builder, "<strong>$strong_text</strong>")
                         .to_owned()
                         .to_string();
-                    a_exp
+                    A_REGEX
                         .replace_all(&builder, "<a href=\"$a_href\">$a_text</a>")
                         .to_owned()
                         .to_string()
                 })
-                .map(|v| format!("    <p>{}</p>", v))
+                .map(|v| format!("<p>{}</p>", v))
                 .fold("".to_string(), |mut acc, paragraph| {
                     acc.push_str(&format!("{}\n", paragraph));
                     acc
@@ -80,13 +111,13 @@ impl Droplet {
 }
 
 fn main() -> Result<(), Error> {
-    let em_regex = Regex::new(r"_(?P<em_text>.*?)_")?;
-    let strong_regex = Regex::new(r"\*\*(?P<strong_text>.*?)\*\*")?;
-    let a_regex = Regex::new(r"\[(?P<a_text>.*?)\]\((?P<a_href>.*?)\)")?;
-
     let d = Droplet::from_file(Path::new("pages/test.yaml"))?;
 
-    if let Some(paragraph_html) = d.content_to_html(em_regex, strong_regex, a_regex) {
+    if let Some(image_src) = d.image_to_html() {
+        print!("{}", image_src);
+    }
+
+    if let Some(paragraph_html) = d.content_to_html() {
         print!("{}", paragraph_html)
     }
 
